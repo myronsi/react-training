526f16b2eaddba3c3e586c448ff076be3f836c06
myronsi <myron.ilchenko@gmail.com>
2024-12-06 22:32:08 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//ReactDOM.render(React.createElement('input', {
+//    placeholder: "Help Text",
+//    onClick: () => console.log("Clicked"),
+//    onMouseEnter: () => console.log("Mouse Over"),
+//}), document.getElementById("app"))
+
+const inputClick = () => console.log("Clicked")
+const mouseOver = () => console.log("Mouse Over")
+const helpText = "Help Text!"
+
+const elements = (<div>
+    <h1>Hello</h1>
+    <input placeholder={helpText}
+    onClick={inputClick} onMouseEnter={mouseOver} />
+    <p>{helpText == "Help Text!" ? "True" : "False"}</p>
+    </div>)
+
+const app = document.getElementById("app")
+
+ReactDOM.render(elements, app)
