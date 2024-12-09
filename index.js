77836633e68e03261428864badce3e9b2323feac
myronsi <myron.ilchenko@gmail.com>
2024-12-09 22:15:51 +0100

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-//ReactDOM.render(React.createElement('input', {
-//    placeholder: "Help Text",
-//    onClick: () => console.log("Clicked"),
-//    onMouseEnter: () => console.log("Mouse Over"),
-//}), document.getElementById("app"))
+ReactDOM.render(React.createElement('input', {
+   placeholder: "Help Text",
+   onClick: () => console.log("Clicked"),
+   onMouseEnter: () => console.log("Mouse Over"),
+}), document.getElementById("app"))
 
 const inputClick = () => console.log("Clicked")
 const mouseOver = () => console.log("Mouse Over")
